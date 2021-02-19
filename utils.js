99ad1a042c6a1f8af2004f91bd5d47665b2dd36e
Andrew Ballantyne <aballant@redhat.com>
2021-02-18 21:56:40 -0500

    Answers

--- utils.js
+++ utils.js
@@ -1,7 +1,11 @@
-const getNames = (list) => list;
-const getSeniors = (list) => list;
-const getRegionCounts = (list) => list;
-const convertDateStamps = (list) => list;
+const getNames = (list) => list.map((d) => d.name);
+const getSeniors = (list) => list.filter((d) => d.title.includes('Senior'));
+const getRegionCounts = (list) =>
+  list.reduce(
+    (acc, d) => ({ ...acc, [d.region]: (acc[d.region] || 0) + 1 }),
+    {},
+  );
+const convertDateStamps = (list) => list.map((d) => ({ ...d, hireDate: new Date(d.hire).toDateString() }));
 
 module.exports = {
   convertDateStamps,
