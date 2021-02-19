681e9b21224fb51d4566eed5a7d1773381e70af6
debsmita1 <debsmita.santra@gmail.com>
2021-02-19 18:41:55 +0530

    use functional programming

--- utils.js
+++ utils.js
@@ -1,7 +1,15 @@
-const getNames = (list) => list;
-const getSeniors = (list) => list;
-const getRegionCounts = (list) => list;
-const convertDateStamps = (list) => list;
+const getNames = (list) => list.map((emp) => emp.userId);
+const getSeniors = (list) => list.filter((emp) => emp.title.includes("Senior"));
+const getRegionCounts = (list) =>
+  list.reduce(
+    (acc, emp) => ({
+      ...acc,
+      [emp.region]: (acc[emp.region] || 0) + 1,
+    }),
+    {}
+  );
+const convertDateStamps = (list) =>
+  list.map((emp) => ({ ...emp, hireDate: new Date(emp.hire).toString() }));
 
 module.exports = {
   convertDateStamps,
